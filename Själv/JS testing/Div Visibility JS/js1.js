9462f67f631f0d4fe3d8adf37be9f639d70d8f8c
TwiceYT <pingzvikke@gmail.com>
2024-10-23 21:41:36 +0200

    update

---
+++ Själv/JS testing/Div Visibility JS/js1.js
@@ -0,0 +1,10 @@
+function showDiv1() {
+    document.getElementById("div1").style.display = "block";
+    document.getElementById("div2").style.display = "none";
+  }
+  
+  function showDiv2() {
+    document.getElementById("div1").style.display = "none";
+    document.getElementById("div2").style.display = "block";
+  }
+  
