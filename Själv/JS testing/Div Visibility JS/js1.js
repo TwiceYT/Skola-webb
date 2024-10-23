b9c2a967e83a1fcc97fe958cd0bfa9908d336aca
TwiceYT <pingzvikke@gmail.com>
2024-10-23 22:38:53 +0200

    update Div visibility

--- Själv/JS testing/Div Visibility JS/js1.js
+++ Själv/JS testing/Div Visibility JS/js1.js
@@ -1,10 +1,37 @@
 function showDiv1() {
-    document.getElementById("div1").style.display = "block";
-    document.getElementById("div2").style.display = "none";
+  var div1 = document.getElementById("Div1");
+  
+  // Kolla om Div1 redan är synlig
+  if (div1.style.display !== "block") {
+    div1.style.display = "block";
+    document.getElementById("Div2").style.display = "none";
+    document.getElementById("Div3").style.display = "none";
+    document.getElementById("Div4").style.display = "none";
+  } else {
+    alert("You are already on Div1");
   }
+}
+
   
   function showDiv2() {
-    document.getElementById("div1").style.display = "none";
-    document.getElementById("div2").style.display = "block";
+    document.getElementById("Div1").style.display = "none";
+    document.getElementById("Div2").style.display = "block";
+    document.getElementById("Div3").style.display = "none";
+    document.getElementById("Div4").style.display = "none";
   }
+
+  function showDiv3() {
+    document.getElementById("Div1").style.display = "none";
+    document.getElementById("Div2").style.display = "none";
+    document.getElementById("Div3").style.display = "block";
+    document.getElementById("Div4").style.display = "none";
+  }
+  
+  function showDiv4() {
+    document.getElementById("Div1").style.display = "none";
+    document.getElementById("Div2").style.display = "none";
+    document.getElementById("Div3").style.display = "none";
+    document.getElementById("Div4").style.display = "block";
+  }
+  
   
