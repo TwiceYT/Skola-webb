9b282834db8ce89e944e53370cbbb6da5d2246ac
Twice <pingzvikke@gmail.com>
2024-10-29 20:57:16 +0100

    update

---
+++ Uppgifter/Övn5/script.js
@@ -0,0 +1,18 @@
+function ShowBanan() {
+    document.getElementById("banan").style.display = "block";
+    document.getElementById("apelsin").style.display = "none";
+    document.getElementById("apple").style.display = "none";
+  }
+
+  function showApelsin() {
+    document.getElementById("banan").style.display = "none";
+    document.getElementById("apelsin").style.display = "block";
+    document.getElementById("apple").style.display = "none";
+  }
+  
+  function showApple() {
+    document.getElementById("banan").style.display = "none";
+    document.getElementById("apelsin").style.display = "none";
+    document.getElementById("apple").style.display = "block";
+  }
+  
